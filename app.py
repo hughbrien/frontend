7054e3d8d14dc63c7c6abd9be755480314c8a454
hughbrien <hugh@hughbrien.com>
2023-06-06 18:09:16 -0400

    Added HTTP client calls

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 import socket
 from datetime import datetime
+import requests
+
 
 
 app = Flask(__name__)
@@ -156,7 +158,40 @@ def live():  # liveness
 def ready():  # readyness
     return '{"status":"ready"}'
 
+@app.route('send_event')
+def send_event(url, payload, headers):
+    custom_event = {
+        "eventType": "Google Cloud Event",
+        "summary": "Host Change Events",
+        "severity": "error",
+        "scope": {
+            "clusters": ["autopilot-cluster-2"],
+            "serviceNames": ["frontend"],
+            "namespaces": ["frontend"]
+        },
+        "details": {
+            "hostname": "server-333332ww",
+            "ipaddress": "192.169.1.1",
+            "Owner": "Hugh Brien",
+            "Email": "hugh@komodor.io"
+        }
+    }
+    posting = requests.post("https://api.komodor.com/mgmt/v1/events",
+                            json=custom_event,
+                            headers={"Content-Type": "application/json",
+                                     "x-api-key": "21527fbe-3fda-4080-b3ec-931a81a361ba"})
+    return posting
+
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
+
+
 
---
+++ client.py
@@ -0,0 +1,26 @@
+import requests
+
+custom_event = {
+  "eventType": "Google Cloud Event",
+  "summary": "Host Change Events",
+  "severity": "error",
+  "scope": {
+    "clusters":["autopilot-cluster-2"],
+    "serviceNames":["frontend"],
+    "namespaces":["frontend"]
+  },
+  "details": {
+    "hostname": "server-333332ww",
+    "ipaddress": "192.169.1.1",
+    "Owner": "Hugh Brien",
+    "Email": "hugh@komodor.io"
+  }
+}
+posting =requests.post("https://api.komodor.com/mgmt/v1/events",
+              json=custom_event,
+              headers={"Content-Type":"application/json",
+                        "x-api-key":"21527fbe-3fda-4080-b3ec-931a81a361ba"})
+
+print(posting)
+
+
