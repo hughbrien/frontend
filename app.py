337f44cfceb1fdcd1a11798207c8d765434a3e7a
hughbrien <hugh@hughbrien.com>
2023-06-06 16:01:53 -0400

    Updating machine event

--- app.py
+++ app.py
@@ -9,6 +9,90 @@ app = Flask(__name__)
 GLOBAL_lIST = []
 SERVICE_VERSION = "2.0.9"
 
+KOMODOR_CUSTOM_EVENT = {
+
+        "eventType": "Google-Cloud-Event-MachineEvent",
+        "summary": "Capture a Log Event from GKE. This is demonstration",
+        "severity": "warning",
+        "scope": {
+            "clusters": ["autopilot-cluster-2"],
+            "serviceNames": ["frontend"],
+            "namespaces": ["frontend"]
+        },
+        "details": {
+            "hostname": "server-333332ww",
+            "ipaddress": "192.169.1.1",
+            "Owner": "Hugh Brien",
+            "Email": "hugh@komodor.io",
+            "timestamp": "Tue, 06 Jun 2023 18:54:58 GMT",
+            "url": "http://34.172.104.248:5000/webhook",
+            "cluster_id": "autopilot-cluster-2",
+            "container_name": "frontend",
+            "location": "us-central1",
+            "namespace_name": "frontend",
+            "pod_name": "frontend-885b7ff57-mdfg",
+            "project_id":"hpb-bank-of-anthos"
+        }
+}
+
+TEST_EVENT = {
+  "handler": "frontend",
+  "incident": {
+    "condition": {
+      "conditionMatchedLog": {
+        "filter": "\"GET /version HTTP/1.1\"",
+        "resourceContainers": [
+          "projects/hpb-bank-of-anthos"
+        ]
+      },
+      "displayName": "Log match condition",
+      "name": "projects/hpb-bank-of-anthos/alertPolicies/10224692307253098708/conditions/10224692307253099895"
+    },
+    "condition_name": "Log match condition",
+    "documentation": {
+      "content": "Get Version Alert",
+      "mime_type": "text/markdown"
+    },
+    "ended_at": "null",
+    "incident_id": "0.mybdstmr76pg",
+    "metadata": {
+      "system_labels": {},
+      "user_labels": {}
+    },
+    "metric": {
+      "displayName": "",
+      "labels": {},
+      "type": ""
+    },
+    "policy_name": "Get Version Alert",
+    "resource": {
+      "labels": {
+        "cluster_name": "autopilot-cluster-2",
+        "container_name": "frontend",
+        "location": "us-central1",
+        "namespace_name": "frontend",
+        "pod_name": "frontend-885b7ff57-mdfgp",
+        "project_id": "hpb-bank-of-anthos"
+      },
+      "type": "k8s_container"
+    },
+    "resource_id": "",
+    "resource_name": "Kubernetes Container labels {cluster_name=autopilot-cluster-2, container_name=frontend, location=us-central1, namespace_name=frontend, pod_name=frontend-885b7ff57-mdfgp, project_id=hpb-bank-of-anthos}",
+    "resource_type_display_name": "Kubernetes Container",
+    "scoping_project_id": "hpb-bank-of-anthos",
+    "scoping_project_number": 301868641236,
+    "started_at": 1686076691,
+    "state": "open",
+    "summary": "Log match condition fired for Kubernetes Container with {cluster_name=autopilot-cluster-2, container_name=frontend, location=us-central1, namespace_name=frontend, pod_name=frontend-885b7ff57-mdfgp, project_id=hpb-bank-of-anthos}.",
+    "url": "https://console.cloud.google.com/monitoring/alerting/incidents/0.mybdstmr76pg?project=hpb-bank-of-anthos"
+  },
+  "timestamp": "Tue, 06 Jun 2023 18:54:58 GMT",
+  "url": "http://34.172.104.248:5000/webhook",
+  "version": "1.2"
+}
+
+
+
 @app.route('/webhook',  methods=['GET','POST'])
 def webhook():  # put changes here
     content_type = request.headers.get('Content-Type')
@@ -36,6 +120,13 @@ def version():  # put application's code here
     json["version"] = SERVICE_VERSION
     json["service_name"] = "frontend"
     json["author"] = "hughpbrien"
+    print(json)
+    return json
+
+@app.route('/translate',  methods=['GET'])
+def translate():  # put application's code here
+    headers = {'content-type': 'application/json'}
+    json = TEST_EVENT
     print(json)
     return json
 
